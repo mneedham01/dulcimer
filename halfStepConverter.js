4e116ec75090fc0b1b2d7e2726606f5e1c126eec
mneedham01 <mneedham@smith.edu>
2023-07-25 11:54:09 -0400

    added everything to one file

--- halfStepConverter.js
+++ halfStepConverter.js
@@ -1,3 +1,4 @@
+export {map, noteGetter, move, dulcimerNotes}
 
 // building map 
 const map = new Map(); 
@@ -24,7 +25,7 @@ function noteGetter(number) {
     }
 }
 
-export function move (note, halfSteps) {
+function move (note, halfSteps) {
 
     // get the number value of the note 
     const num = map.get(note)
@@ -32,7 +33,7 @@ export function move (note, halfSteps) {
     // set up newNum 
     let newNum 
 
-    if (num + halfSteps > 12) {
+    if (num + halfSteps < 12) {
         newNum = num + halfSteps
     } else {
         //won't move more than an octave
@@ -41,3 +42,39 @@ export function move (note, halfSteps) {
 
     return noteGetter(newNum)
 }
+
+function dulcimerNotes(note){
+
+    let list = []
+
+    // W-W-H-W-W-H-W
+    // tonic (0)
+    list.push(note)
+    // W (1)
+    list.push(move(note,2))
+    //W (2)
+    list.push(move(note,4))
+    //H (3)
+    list.push(move(note,5))
+    //W (4)
+    list.push(move(note,7))
+    //W (5)
+    list.push(move(note,9))
+    //W (6)
+    list.push(move(note,10))
+    //(6.5)
+    list.push(move(note,11))
+    // W (7)
+    list.push(move(note,12))
+    // W (8)
+    list.push(move(note,14))
+    // W (9)
+    list.push(move(note,16))
+    // H (10)
+    list.push(move(note,17))
+    // W (11)
+    list.push(move(note,19))
+    // W (12)
+    list.push(move(note,21))
+}
+
