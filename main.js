d6cacb633a328ba867f748d775bf6d32092918fc
mneedham01 <mneedham@smith.edu>
2023-10-28 17:26:52 -0400

    created loop

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-// building map 
-const map = new Map(); 
+// building map
+const map = new Map();
 
 map.set("C", 1);
 map.set("C#", 2);
@@ -26,18 +26,18 @@ function noteGetter(number) {
 
 function move (note, halfSteps) {
 
-    // get the number value of the note 
+    // get the number value of the note
     const num = map.get(note)
 
-    // set up newNum 
-    let newNum 
+    // set up newNum
+    let newNum
 
     if (num + halfSteps <= 12) {
         newNum = num + halfSteps
     } else {
         while(num + halfSteps -12 > 12 ){
-            halfSteps = halfSteps -12 
-        }   
+            halfSteps = halfSteps -12
+        }
         newNum = num + halfSteps - 12
     }
 
@@ -45,43 +45,22 @@ function move (note, halfSteps) {
 }
 
 function dulcimerNotes(note){
-
+    // limiting to first octave (for now)
     let list = []
 
     // W-W-H-W-W-H-W
-    // tonic (0)
-    list.push(note)
-    // W (1)
-    list.push(move(note,2))
-    //W (2)
-    list.push(move(note,4))
-    //H (3)
-    list.push(move(note,5))
-    //W (4)
-    list.push(move(note,7))
-    //W (5)
-    list.push(move(note,9))
-    //W (6)
-    list.push(move(note,10))
-    //(6.5)
-    list.push(move(note,11))
-    // W (7)
-    list.push(move(note,12))
-    // W (8)
-    list.push(move(note,14))
-    // W (9)
-    list.push(move(note,16))
-    // H (10)
-    list.push(move(note,17))
-    // W (11)
-    list.push(move(note,19))
-    // W (12)
-    list.push(move(note,21))
+    // 0, 1, 2, 3, 4, 5, 6
+    for (i = 0; i < 7; i++) {
+        list.push(note, i)
+    }
+    // add 6.5 and 7
+    list.push(note, 6.5)
+    list.push(note, 7)
 
     return list
 }
 
-//build a major chord 
+//build a major chord
 function major (note){
 
     let list = [note]
@@ -110,38 +89,149 @@ function minor (note) {
 }
 
 function nextIndex(i) {
-    if (i ==2){
-        return 0 
-    } else{
-        return i + 1
+    if (parseInt(i) == 2){
+        return 0
+    } else {
+        return parseInt(i) + 1
     }
 }
 
+function printTab(list){
+    console.log("Available chord positions: ")
+    // looping through the tab
+    for (i in list){
+        str = "\nBass: " + list[i][0]
+        str += "\nMiddle: " + list[i][1]
+        str +="\nTop: "+ list[i][2]
+        console.log(str)
+    }
+    console.log("\n")
+}
+
 function main(string1, string2, string3){
-    list1 = dulcimerNotes(string1)
-    list2 = dulcimerNotes(string2)
-    list3 = dulcimerNotes(string3)
+
+    const list1 = dulcimerNotes(string1)
+    const list2 = dulcimerNotes(string2)
+    const list3 = dulcimerNotes(string3)
+
 
     //loop through all notes
     for (const [key] of map){
 
-        // major chord 
+        const list = []
+
+        // major chord
         const majorChord = major(key)
 
-        // loop through major chord 
+        //print out message
+        console.log(key + " Major Chord: " + majorChord[0]+ ", " + majorChord[1]+ ", "+ majorChord[2]+".")
+
+        // loop through major chord
         for (i in majorChord){
 
             note = majorChord[i]
 
-            // check whether note is available in bass string 
-            if (list1.includes(note)) {
-                // if the bass string contains the note, then check if the middle string contains the next note 
+            const nextNote = majorChord[nextIndex(i)]
 
+            const nextNext = majorChord[nextIndex(nextIndex(i))]
+
+            // allowed to do just tonic and fifth
+            if (i==0){
+                if (list1.includes(note)){
+                    // if bass string contains tonic
+                    let bassTab = list1.indexOf(note)
+                    if (list2.contains(note)){
+                        // if middle contains tonic
+                        let middleTab = list2.indexOf(note)
+                        if (list3.contains(note)){
+                            // if top contains tonic
+                            let topTab = list3.indexOf(note)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                        if (list3.contains(nextNext)) {
+                            // if top contains fifth
+                            let topTab = list3.indexOf(nextNext)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                    } if (list2.contains(nextNext)){
+                        // if middle contains fifth
+                        let middleTab = list2.indexOf(nextNext)
+                        if (list3.contains(note)){
+                            // if top contains tonic
+                            let topTab = list3.indexOf(note)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                        if (list3.contains(nextNext)) {
+                            // if top contains fifth
+                            let topTab = list3.indexOf(nextNext)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                    }
+                }
+                if (list2.includes(note)){
+                    // if bass string contains tonic
+                    let bassTab = list1.indexOf(note)
+                    if (list2.contains(note)){
+                        // if middle contains tonic
+                        let middleTab = list2.indexOf(note)
+                        if (list3.contains(note)){
+                            // if top contains tonic
+                            let topTab = list3.indexOf(note)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                        if (list3.contains(nextNext)) {
+                            // if top contains fifth
+                            let topTab = list3.indexOf(nextNext)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                    } if (list2.contains(nextNext)){
+                        // if middle contains fifth
+                        let middleTab = list2.indexOf(nextNext)
+                        if (list3.contains(note)){
+                            // if top contains tonic
+                            let topTab = list3.indexOf(note)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                        if (list3.contains(nextNext)) {
+                            // if top contains fifth
+                            let topTab = list3.indexOf(nextNext)
+                            list.push(bassTab, middleTab, topTab)
+                        }
+                    }
+                }
+                if (list3.includes(note)){
+
+                }
+            }
+            // check whether note is available in bass string
+            if (list1.includes(note)) {
+                // if the bass string contains the note, then check if the middle string contains the next note
+                let bassTab = list1.indexOf(note)
+                if (list2.includes(nextNote)){
+                    //if the middle string contains the next note, check if the top string contains the final note
+                    let middleTab = list2.indexOf(nextNote)
+                    if (list3.includes(nextNext)){
+                        //if the top string contains the final note, set it as a tab triplet
+                        let topTab = list3.indexOf(nextNext)
+                        list.push([bassTab, middleTab, topTab])
+                    }
+                } else if (list2.includes(nextNext)){
+                    // if the middle string contains the next next note, check if the top string contains next note
+                    let middleTab = list2.indexOf(nextNext)
+                    if (list3.includes(nextNote)){
+                        //if the top string contains the final note, set it as a tab triplet
+                        let topTab = list3.indexOf(nextNote)
+                        list.push([bassTab, middleTab, topTab])
+                    }
+                }
             }
         }
+        //check list for finger width
+
+        printTab(list)
     }
 }
 
-main("D", "A", "A")
+main("D", "A", "D")
 
 
